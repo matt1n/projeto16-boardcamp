6d9e6be105875e05ad62ca2a6cc1a9904f88fc19
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-12-11 00:34:50 -0300

    feat: customers routes

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,5 +1,7 @@
 import { connection } from "../database/database.js"
 
+//                              FAZER A QUERY DO GET
+
 export async function getCustomers(req,res){
     try {
         const costumers = await connection.query(`
@@ -15,10 +17,47 @@ export async function getCustomers(req,res){
     }
 }
 
-export async function getCustomersById(req,res){
+export async function getCustomerById(req,res){
     const customer = req.customer
     try {
         res.send(customer)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+export async function postCustomer(req,res){
+    const {name, phone, cpf, birthday} = res.locals.body
+    try{
+        await connection.query(`
+            INSERT INTO
+                customers
+                (name, phone, cpf, birthday)
+            VALUES
+                ($1, $2, $3, $4)
+        `,[name, phone, cpf, birthday])
+        res.sendStatus(201)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+export async function putCustomer(req,res){
+    const id = req.id
+    const {name, phone, cpf, birthday} = res.locals.body
+
+    try {
+        await connection.query(`
+            UPDATE
+                customers
+            SET
+                name= ($1), phone= ($2), cpf= ($3), birthday=($4)
+            WHERE
+                id = ($5)
+        `,[name, phone, cpf, birthday, id])
+        res.sendStatus(201)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getCustomers, getCustomersById } from "../controllers/customersController.js";
-import { getCustomerByIdValidation } from "../middlewares/customersMiddlewares.js";
+import { getCustomers, getCustomerById, postCustomer, putCustomer } from "../controllers/customersController.js";
+import { CustomerBodyAndIdValidation, CustomerBodyValidation, getCustomerByIdValidation } from "../middlewares/customersMiddlewares.js";
 
 const router = Router()
 
 router.get('/customers', getCustomers)
-router.get('/customers/:id', getCustomerByIdValidation, getCustomersById)
+router.get('/customers/:id', getCustomerByIdValidation, getCustomerById)
+router.post('/customers', CustomerBodyValidation, postCustomer)
+router.put('/customers/:id', CustomerBodyAndIdValidation, putCustomer)
 
 export default router
