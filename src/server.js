8d2fc454bcddbcbfa6d3ec9c3e8f6847353201ff
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-12-10 21:54:58 -0300

    feat: games routes whithout search feature

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import categoriesRouters from "./routes/categoriesRoutes.js"
+import gamesRouters from "./routes/gamesRoutes.js"
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 app.use(categoriesRouters)
+app.use(gamesRouters)
 
 const port = process.env.PORT || 4000
 
